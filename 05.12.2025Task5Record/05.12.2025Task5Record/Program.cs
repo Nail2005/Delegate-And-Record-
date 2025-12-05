4016c7a9b8bfd3b5b62c87d6bc3d6e74c60cb00b
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:47:46 +0400

    5 Task

---
+++ 05.12.2025Task5Record/05.12.2025Task5Record/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Order order1 = new Order(1, "Ali", 99.99m);
+        Order order2 = new Order(1, "Ali", 99.99m);
+
+        if (order1 == order2)
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
+    }
+}
+
+public record Order(int orderId, string customerName, decimal totalAmount);
