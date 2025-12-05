dd29cda0be39d70d8c708a15da4dd759e1223d7e
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:45:22 +0400

    3 Task

---
+++ 05.12.2025Task3Delegate/05.12.2025Task3Delegate/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int> calculateAge = birhtyear => DateTime.Now.Year - birhtyear;
+
+        int age = calculateAge(2003);
+
+        Console.WriteLine($"Age: {age}");
+    }
+}
