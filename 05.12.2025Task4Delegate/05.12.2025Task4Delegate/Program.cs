7eeb004b4679a4ed328de08fcaaa408f8dbe6675
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:46:30 +0400

    4 Task

---
+++ 05.12.2025Task4Delegate/05.12.2025Task4Delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<double> salaries = new List<double> { 500, 600, 700, 1100, 1200 };
+
+        Predicate<double> predicate = num => num > 1000;
+
+        List<double> filteredSalary = salaries.FindAll(predicate);
+
+        foreach (var salary in filteredSalary)
+        {
+            Console.WriteLine(salary);
+        }
+    }
+}
