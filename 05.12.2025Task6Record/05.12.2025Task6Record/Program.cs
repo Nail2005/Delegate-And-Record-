f2591014956cdc6295b167e36d6d66f35b775aec
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:48:45 +0400

    6 Task

---
+++ 05.12.2025Task6Record/05.12.2025Task6Record/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public record Student(string Name, string Surname, int Score);
+
+class Program
+{
+    static void Main()
+    {
+        var student1 = new Student("Leyla", "Muradova", 95);
+
+        var (name, surname, score) = student1;
+
+        Console.WriteLine($"Ad:{name} Soyad:{surname} Bal:{score}");
+    }
+}
