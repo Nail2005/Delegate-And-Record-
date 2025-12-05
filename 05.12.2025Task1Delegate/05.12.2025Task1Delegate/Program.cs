c18302f9f29c7085e87d8869b8dbbd1bdbb02a04
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:24:48 +0400

    1 Task

---
+++ 05.12.2025Task1Delegate/05.12.2025Task1Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public delegate void SendNotification(string msg);
+
+class Program
+{
+    public static void SendEmail(string mesaj)
+    {
+        Console.WriteLine($"Email göndərildi: {mesaj}");
+    }
+
+    public static void ShowAlert(string mesaj)
+    {
+        Console.WriteLine($"Ekranda bildiriş çıxdı: {mesaj}");
+    }
+
+
+    static void Main(string[] args)
+    {
+        SendNotification notification;
+        notification = SendEmail;
+
+        notification+= ShowAlert;
+
+        notification("Yeni mesajınız var!");
+
+    }
+    
+}
