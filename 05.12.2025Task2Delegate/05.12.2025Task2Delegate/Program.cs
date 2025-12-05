04efa8ffa87348915c8c4f80cc2f74d862c530cd
Nail2005 <githubnail2005@gmail.com>
2025-12-05 15:43:59 +0400

    2 Task

---
+++ 05.12.2025Task2Delegate/05.12.2025Task2Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Action<string, int> updateStock = (productName, count) =>
+        {
+            Console.WriteLine($"Anbarda {productName} qalığı yenilendi. Yeni say: {count}");
+
+        };
+        updateStock("Book", 50);
+    }
+}
